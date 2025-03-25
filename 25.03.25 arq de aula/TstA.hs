ffc1fece03e8a1075dc696bf7bdfb5f48ad54c95
kevin willian <kevin_willian@hotmail.com.br>
2025-03-25 14:41:26 -0300

    parece um object, mas versao haskell

---
+++ 25.03.25 arq de aula/TstA.hs
@@ -0,0 +1,6 @@
+module TstA where
+
+    data Cliente = OrgGov String
+                | Empresa String Integer String String
+                | Individuo String String Bool
+                deriving Show
