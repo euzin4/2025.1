a2dfae0f6a327a395137d0112484468b23ce47f8
kevin willian <kevin_willian@hotmail.com.br>
2025-04-11 17:28:30 -0300

    Update correcaoP1.hs

--- linguagens de programação/correcaoP1.hs
+++ linguagens de programação/correcaoP1.hs
@@ -5,14 +5,14 @@ type Estudante = (Matricula, Nome, Notas)
 type Turma = [Estudante]
 
 exemplo :: Turma
-exemplo = [(1, "Pedro", (5.0,7.5,4.5))
+exemplo =   [(1, "Pedro", (5.0,7.5,4.5))
             ,(2, "Maria", (9.0,8.0,10.0))
             ,(3, "Joao", (3.0,7.0,4.5))
             ,(4, "Paulo", (7.0,5.0,9.5))
             ,(5, "Ana", (8.5,8.0,9.0))
             ]
 
---resposta a)
+--resposta a) (nao funcionando)
 somaMedias :: Turma -> Double
 somaMedias [] = 0
 somaMedias ((_, _, (n1, n2, n3)) : xs) = (n1, n2, n3) / 3 + somaMedias xs
