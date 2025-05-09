bd19346f4fbdcca3cb214399e2c6bbd29bd37550
kevin willian <kevin_willian@hotmail.com.br>
2025-05-09 16:39:35 -0300

    Update Interpreter.hs

--- linguagens de programação/06.05.25 arq de aula/Interpreter.hs
+++ linguagens de programação/06.05.25 arq de aula/Interpreter.hs
@@ -10,6 +10,7 @@ step (Add e1 e2) = Add (step e1) e2
 step (And BTrue e2) = e2
 step (And Bfalse e2) = Bfalse
 step (And e1 e2) = And (step e1) e2
--- step (If BTrue e1 e2) = e1
--- step (If Bfalse e1 e2) = e2
--- step (If e e1 e2) = If (step e) e1 e2
+
+step (If BTrue e1 e2) = e1
+step (If Bfalse e1 e2) = e2
+step (If e e1 e2) = If (step e) e1 e2
